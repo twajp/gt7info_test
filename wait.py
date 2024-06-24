ac4ddd62d8a82c9e19daf080c61f110c39cdcc31
Tomoki Akasaka <64235761+twajp@users.noreply.github.com>
2024-06-24 16:43:59 +0900

    Update wait.py

--- wait.py
+++ wait.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta, timezone
+from dateutil import tz
 from time import sleep
 import requests
 
 t = 0
 while (True):
     url = 'https://raw.githubusercontent.com/ddm999/gt7info/web-new/_data/used/'
-    JST = timezone(timedelta(hours=+9), 'JST')
+    JST = tz.gettz("Asia/Tokyo")
     filename = datetime.now(JST).date().strftime('%y-%m-%d')+'.csv'
     urlData = requests.get(url+filename).content
     if urlData != b'404: Not Found':

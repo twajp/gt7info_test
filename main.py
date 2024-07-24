925fcf06e4e66786f0f5cbda8700b5324d1145f8
Tomoki Akasaka <64235761+twajp@users.noreply.github.com>
2024-07-24 14:51:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def LoadCSV(directory, filename):
-    url = f"https://raw.githubusercontent.com/ddm999/gt7info/web-new/_data/{directory}{filename}"
+    url = f'https://raw.githubusercontent.com/ddm999/gt7info/web-new/_data/{directory}{filename}'
     download = requests.get(url)
     decoded_content = download.content.decode('utf-8')
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
@@ -29,7 +29,7 @@ def LoadJSON(url):
 def MakeNewCarList(data, carList, makerList):
     res = []
     for i in range(len(data)):
-        if data[i][2] == "new":
+        if data[i][2] == 'new':
             for j in range(len(carList)):
                 if data[i][0] == carList[j][0]:
                     for k in range(len(makerList)):
@@ -37,8 +37,8 @@ def MakeNewCarList(data, carList, makerList):
                             makername = makerList[k][1]
                             carid = carList[j][0]
                             carname = carList[j][1]
-                            price = format(int(data[i][1]), ",")
-                            price_in_jpy = format(int(data[i][1])*100, ",")
+                            price = format(int(data[i][1]), ',')
+                            price_in_jpy = format(int(data[i][1])*100, ',')
 
                             try:
                                 carYear = int(carname[-2:])
@@ -50,20 +50,20 @@ def MakeNewCarList(data, carList, makerList):
                                 isOld = None
 
                             res.append(
-                                {"makername": makername, "carid": carid, "carname": carname, "price": price, "price_in_jpy": price_in_jpy, "isOld": isOld})
+                                {'makername': makername, 'carid': carid, 'carname': carname, 'price': price, 'price_in_jpy': price_in_jpy, 'isOld': isOld})
     return res
 
 
 def UpdateDB(dealer):
     for day in reversed(data):
         for car in day[dealer]:
-            db[dealer][car["carid"]] = {
-                "makername": car["makername"],
-                "carname": car["carname"],
-                "price": car["price"],
-                "price_in_jpy": car["price_in_jpy"],
-                "isOld": car["isOld"],
-                "lastAppeared": day["date"],
+            db[dealer][car['carid']] = {
+                'makername': car['makername'],
+                'carname': car['carname'],
+                'price': car['price'],
+                'price_in_jpy': car['price_in_jpy'],
+                'isOld': car['isOld'],
+                'lastAppeared': day['date'],
             }
 
 
@@ -71,18 +71,18 @@ def Select(cars_dict, percentage):
     num_top_cars = int(len(cars_dict) / 100 * percentage)
     db_top = dict(list(cars_dict.items())[:num_top_cars])
     for car_id, car_info in db_top.items():
-        db_top[car_id].update({"sinceLastAppeared": (today - datetime.strptime(car_info['lastAppeared'], '%Y/%m/%d').date()).days})
+        db_top[car_id].update({'sinceLastAppeared': (today - datetime.strptime(car_info['lastAppeared'], '%Y/%m/%d').date()).days})
 
     return db_top
 
 
-db = LoadJSON(f"https://raw.githubusercontent.com/twajp/gt7info_test/gh-pages/db.json")
-carList = LoadCSV("db/", "cars.csv")
-makerList = LoadCSV("db/", "maker.csv")
+db = LoadJSON(f'https://raw.githubusercontent.com/twajp/gt7info_test/gh-pages/db.json')
+carList = LoadCSV('db/', 'cars.csv')
+makerList = LoadCSV('db/', 'maker.csv')
 today = datetime.now(timezone.utc).date()
-JST = tz.gettz("Asia/Tokyo")
-timestamp = datetime.now(timezone.utc).strftime("%Y/%-m/%-d %-H:%M") + " (UTC)"
-timestamp_jp = datetime.now(timezone.utc).astimezone(JST).strftime("%Y/%-m/%-d %-H:%M") + " (JST)"
+JST = tz.gettz('Asia/Tokyo')
+timestamp = datetime.now(timezone.utc).strftime('%Y/%-m/%-d %-H:%M') + ' (UTC)'
+timestamp_jp = datetime.now(timezone.utc).astimezone(JST).strftime('%Y/%-m/%-d %-H:%M') + ' (JST)'
 # start_date = datetime.date(year=2022,month=6,day=28)
 # how_many_days = (today-start_date).days + 1
 how_many_days = 14
@@ -90,58 +90,58 @@ data = []
 
 for i in range(how_many_days):
     date_to_import = today - timedelta(i)
-    filename = date_to_import.strftime("%y-%m-%d")+".csv"
+    filename = date_to_import.strftime('%y-%m-%d')+'.csv'
 
-    data_used = LoadCSV("used/", filename)
-    data_legend = LoadCSV("legend/", filename)
+    data_used = LoadCSV('used/', filename)
+    data_legend = LoadCSV('legend/', filename)
 
     list_used = MakeNewCarList(data_used, carList, makerList)
     list_legend = MakeNewCarList(data_legend, carList, makerList)
 
     data.append({
-        "id": date_to_import.strftime("%Y%m%d"),
-        "date": date_to_import.strftime("%Y/%-m/%-d"),
-        "used": list_used,
-        "legend": list_legend,
+        'id': date_to_import.strftime('%Y%m%d'),
+        'date': date_to_import.strftime('%Y/%-m/%-d'),
+        'used': list_used,
+        'legend': list_legend,
     })
-    print(f"Day {i}")
+    print(f'Day {i}')
 
 
-UpdateDB("used")
-UpdateDB("legend")
+UpdateDB('used')
+UpdateDB('legend')
 
-db["used"] = dict(sorted(db["used"].items(), key=lambda item: (today - datetime.strptime(item[1]["lastAppeared"], "%Y/%m/%d").date()).days, reverse=True))
-db["legend"] = dict(sorted(db["legend"].items(), key=lambda item: (today - datetime.strptime(item[1]["lastAppeared"], "%Y/%m/%d").date()).days, reverse=True))
+db['used'] = dict(sorted(db['used'].items(), key=lambda item: (today - datetime.strptime(item[1]['lastAppeared'], '%Y/%m/%d').date()).days, reverse=True))
+db['legend'] = dict(sorted(db['legend'].items(), key=lambda item: (today - datetime.strptime(item[1]['lastAppeared'], '%Y/%m/%d').date()).days, reverse=True))
 
 db_top = {}
-db_top["used"] = Select(db["used"], percentage=20)
-db_top["legend"] = Select(db["legend"], percentage=10)
+db_top['used'] = Select(db['used'], percentage=20)
+db_top['legend'] = Select(db['legend'], percentage=10)
 
 
-env = Environment(loader=FileSystemLoader("."))
-template = env.get_template("template.html")
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('template.html')
 
-rendered = template.render({"data": data, "price": "global", "timestamp": timestamp, "db_top": db_top})
-rendered_jp = template.render({"data": data, "price": "jp", "timestamp": timestamp_jp, "db_top": db_top})
-rendered_simple = template.render({"data": data, "price": "simple", "timestamp": timestamp, "db_top": db_top})
+rendered = template.render({'data': data, 'price': 'global', 'timestamp': timestamp, 'db_top': db_top})
+rendered_jp = template.render({'data': data, 'price': 'jp', 'timestamp': timestamp_jp, 'db_top': db_top})
+rendered_simple = template.render({'data': data, 'price': 'simple', 'timestamp': timestamp, 'db_top': db_top})
 
-if not os.path.exists("html"):
-    os.makedirs("html")
+if not os.path.exists('html'):
+    os.makedirs('html')
 
-with open("html/index.html", "w") as f:
+with open('html/index.html', 'w') as f:
     f.write(rendered)
 
-with open("html/jp.html", "w") as f:
+with open('html/jp.html', 'w') as f:
     f.write(rendered_jp)
 
-with open("html/simple.html", "w") as f:
+with open('html/simple.html', 'w') as f:
     f.write(rendered_simple)
 
-with open("html/data.json", "w") as f:
+with open('html/data.json', 'w') as f:
     json.dump(data, f, indent=2, ensure_ascii=False)
 
-with open("html/db.json", "w") as f:
+with open('html/db.json', 'w') as f:
     json.dump(db, f, indent=2, ensure_ascii=False)
 
-copyfile("style.css", "html/style.css")
-copyfile("script.js", "html/script.js")
+copyfile('style.css', 'html/style.css')
+copyfile('script.js', 'html/script.js')
